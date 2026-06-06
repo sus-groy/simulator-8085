84428d628c66df5931ab14a9dce9c5fe2fca9799
PASTUSEDBY <56929455+PASTUSEDBY@users.noreply.github.com>
2026-06-06 19:29:24 +0530

    fix: extern in declaration and actually allocate in the source file

--- include/memory/registers.h
+++ include/memory/registers.h
@@ -8,17 +8,17 @@ int8_t get_data_8(Reg8 *reg);  // get 8-bit data
 void set_data_8(Reg8 *reg, int8_t new_data_8b);  // set new 8-bit data
 
 // start of 8-bit registers file:
-Reg8 reg_A;  // accumulator
-Reg8 reg_F;  // flags register
+extern Reg8 reg_A;  // accumulator
+extern Reg8 reg_F;  // flags register
 
-Reg8 reg_B;
-Reg8 reg_C;
+extern Reg8 reg_B;
+extern Reg8 reg_C;
 
-Reg8 reg_D;
-Reg8 reg_E;
+extern Reg8 reg_D;
+extern Reg8 reg_E;
 
-Reg8 reg_H;
-Reg8 reg_L;
+extern Reg8 reg_H;
+extern Reg8 reg_L;
 // ^ end of 8-bit registers file
 
 typedef struct Reg16 Reg16;  // register that can contain 16-bit data word
@@ -27,6 +27,6 @@ typedef struct Reg16 Reg16;  // register that can contain 16-bit data word
 int16_t get_data_16(Reg16 *reg);
 void set_data_16(Reg16 * reg, int16_t new_data_16b);
 
-Reg16 reg_SP;    // stack pointer
-Reg16 reg_PC;    // programme counter
+extern Reg16 reg_SP;    // stack pointer
+extern Reg16 reg_PC;    // programme counter
 // only two 16-bit registers in system
--- src/memory/registers.c
+++ src/memory/registers.c
@@ -17,3 +17,15 @@ HELPERS(int8_t, 8)
 HELPERS(int16_t, 16)
 
 #undef HELPERS
+
+Reg8 reg_A;  
+Reg8 reg_F;  
+Reg8 reg_B;
+Reg8 reg_C;
+Reg8 reg_D;
+Reg8 reg_E;
+Reg8 reg_H;
+Reg8 reg_L;
+
+Reg16 reg_SP;    
+Reg16 reg_PC;
